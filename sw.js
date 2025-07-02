bc433bc27ddf9dbe0dea3ba1869f58d8b3efaddc
akaucup <40891013+akaucup@users.noreply.github.com>
2025-07-02 22:19:06 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,28 @@
 // sw.js
-const CACHE_NAME = 'offline-cache-v1';
-const URLS_TO_CACHE = [
-  '/',
-  '/index.html',
-];
+const CACHE_NAME = "offline-cache-v1";
+const URLS_TO_CACHE = ["/", "/index.html", "/src/app.js", "/src/style.css", "/assets/thumb.jpeg"];
 
 // Install event
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(URLS_TO_CACHE);
     })
   );
 });
 
 // Fetch event
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      // Kembalikan dari cache jika ada, jika tidak ambil dari jaringan
-      return response || fetch(event.request);
-    }).catch(() => {
-      // Optional: fallback page jika tidak ada koneksi dan tidak ada cache
-      return caches.match('/index.html');
-    })
+    caches
+      .match(event.request)
+      .then((response) => {
+        // Kembalikan dari cache jika ada, jika tidak ambil dari jaringan
+        return response || fetch(event.request);
+      })
+      .catch(() => {
+        // Optional: fallback page jika tidak ada koneksi dan tidak ada cache
+        return caches.match("/index.html");
+      })
   );
 });
